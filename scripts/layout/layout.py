acefe7c33d45796727f3437f4094b08f73c73156
Matt Books <mattebooks@gmail.com>
2018-04-01 16:36:45 -0700

    Equal spacing for mounting holes and fill PCB ground

--- scripts/layout/layout.py
+++ scripts/layout/layout.py
@@ -185,21 +185,48 @@ HOLE_OFFSET_C_2X = HOLE_WIDTH * math.cos(TILT * 2)
 TOP_HOLE_SPACE = 6 * K_WIDTH
 BOTTOM_HOLE_SPACE = 6 * K_WIDTH - 2 * HOLE_WIDTH
 
+top_left_hole = pcbnew.wxPoint(
+    top_left.x + HOLE_OFFSET_S,
+    top_left.y - HOLE_OFFSET_C
+)
+
+top_right_hole = pcbnew.wxPoint(
+    top_right.x - HOLE_OFFSET_S,
+    top_right.y - HOLE_OFFSET_C
+)
+
+bottom_left_hole = pcbnew.wxPoint(
+    bottom_left.x + HOLE_OFFSET_S_NEG,
+    bottom_left.y + HOLE_OFFSET_C_NEG
+)
+
+bottom_right_hole = pcbnew.wxPoint(
+    bottom_right.x - HOLE_OFFSET_S_NEG,
+    bottom_right.y + HOLE_OFFSET_C_NEG
+)
+
+bottom_middle_hole = pcbnew.wxPoint(
+    bottom_middle.x,
+    bottom_middle.y + HOLE_OFFSET_C_2X
+)
+
+top_delta = top_right.x - top_left.x;
+
 mounting_holes = [
-    [top_left.x + HOLE_OFFSET_S, top_left.y - HOLE_OFFSET_C],
-    [top_right.x - HOLE_OFFSET_S, top_right.y - HOLE_OFFSET_C],
+    [top_left_hole.x, top_left_hole.y],
+    [top_right_hole.x, top_right_hole.y],
 
-    [top_left.x + HOLE_OFFSET_S + TOP_HOLE_SPACE, top_left.y - HOLE_OFFSET_C],
-    [top_right.x - HOLE_OFFSET_S - TOP_HOLE_SPACE, top_right.y - HOLE_OFFSET_C],
+    [top_left_hole.x + top_delta / 3.0, top_left_hole.y],
+    [top_right_hole.x - top_delta / 3.0, top_right_hole.y],
 
-    [bottom_left.x + HOLE_OFFSET_S_NEG, bottom_left.y + HOLE_OFFSET_C_NEG],
-    [bottom_right.x - HOLE_OFFSET_S_NEG, bottom_right.y + HOLE_OFFSET_C_NEG],
-    [bottom_middle.x, bottom_middle.y + HOLE_OFFSET_C_2X],
+    [bottom_left_hole.x, bottom_left_hole.y],
+    [bottom_right_hole.x, bottom_right_hole.y],
+    [bottom_middle_hole.x, bottom_middle_hole.y],
 
-    [bottom_left.x + HOLE_OFFSET_S_NEG + BOTTOM_HOLE_SPACE * math.cos(TILT),
-     bottom_left.y + HOLE_OFFSET_C_NEG + BOTTOM_HOLE_SPACE * math.sin(TILT)],
-    [bottom_right.x - HOLE_OFFSET_S_NEG - BOTTOM_HOLE_SPACE * math.cos(TILT),
-     bottom_right.y + HOLE_OFFSET_C_NEG + BOTTOM_HOLE_SPACE * math.sin(TILT)],
+    [(bottom_left_hole.x + bottom_middle_hole.x) / 2.0,
+     (bottom_left_hole.y + bottom_middle_hole.y) / 2.0],
+    [(bottom_right_hole.x + bottom_middle_hole.x) / 2.0,
+     (bottom_right_hole.y + bottom_middle_hole.y) / 2.0],
 
     # [top_left.x + HOLE_OFFSET_X, top_left.y - HOLE_OFFSET_Y],
     # [top_left.x + 90000000 + HOLE_OFFSET_X, top_left.y - HOLE_OFFSET_Y],
